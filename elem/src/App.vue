2706d8a4504d775bcea02bf5e81cee1b99db035a
Aprilwyh <1416402295@qq.com>
2020-04-10 22:14:08 +0800

    添加初始结构

--- elem/src/App.vue
+++ elem/src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
+// 引用header组件
+import header from './components/header/header.vue';
 export default {
-  name: 'App'
-}
+  name: 'App',
+  components: {
+    'v-header': header
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+#app .tab {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+}
+#app .tab .tab-item {
+  flex: 1;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ elem/src/components/header/header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script>
+// 导出该组件
+export default {};
+</script>
+<style>
+</style>
