cd955ed6d804ebe95359aa47182e75df5202a40a
Aprilwyh <1416402295@qq.com>
2020-08-08 22:29:03 +0800

    添加vue-loader，新增webpack相关配置

--- vue_todo/webpack.config.js
+++ vue_todo/webpack.config.js
@@ -1,12 +1,26 @@
-// 导入path模块
+// 导入node的path模块
 const path = require('path')
+
+// 引入vue-loader的插件
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   // 打包入口
-  enter: './src/main.js',
+  entry: './src/main.js',
   // 打包出口
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  // 打包规则
+  module: {
+    rules: [{
+      test: /\.vue$/, // 正则匹配.vue后缀名文件
+      loader: 'vue-loader' // 匹配到了.vue文件就用vue-loader进行打包
+    }]
+  },
+  // 插件
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 }
