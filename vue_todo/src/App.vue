f489bd5374e9922ada0b24af3fc618eaf724586f
Aprilwyh <1416402295@qq.com>
2020-08-18 22:16:13 +0800

    添加svg，todo相关组件，新增相关配置

--- vue_todo/src/App.vue
+++ vue_todo/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <main-header></main-header>
+    <main-todo></main-todo>
   </div>
 </template>
 
@@ -8,11 +9,14 @@
   import './assets/styles/global.styl'
 
   import mainHeader from './components/mainHeader.vue'
+  import mainTodo from './components/mainTodo/mainTodo.vue'
 
   export default {
     name: 'App',
     components: {
-      mainHeader
+      // 挂载
+      mainHeader,
+      mainTodo
     }
   }
 </script>
