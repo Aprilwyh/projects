331e01c9a3bde7531ee27e68ede2dfd0cb339788
Aprilwyh <1416402295@qq.com>
2020-09-21 21:41:41 +0800

    user-detail切换菜单

--- Wblog/src/router/index.js
+++ Wblog/src/router/index.js
@@ -1,55 +1,70 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
 // 解决 ElementUI 导航栏中的 vue-router 在 3.0 以上版本重复点菜单报错问题
-const originalPush = Router.prototype.push
+const originalPush = Router.prototype.push;
 Router.prototype.push = function push(location) {
-  return originalPush.call(this, location).catch(err => err)
-}
+  return originalPush.call(this, location).catch(err => err);
+};
 
 // 同步加载
 // import main from '@/components/main'
 // import login from '@/components/container'
 // 异步加载
-const main = () => import('@/components/main')
-const login = () => import('@/components/container')
+const main = () => import("@/components/main");
+const login = () => import("@/components/container");
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
-      path: '/',
-      redirect: '/home' // 重定向
+      path: "/",
+      redirect: "/home" // 重定向
     },
     {
-      path: '/home',
-      name: 'main',
+      path: "/home",
+      name: "main",
       component: main
     },
     {
-      path: '/user',
-      name: 'user',
-      component: () => import('@/views/user/user.vue'),
+      path: "/user",
+      name: "user",
+      component: () => import("@/views/user/user.vue"),
       children: [
         {
-          path: 'settings',
-          name: 'settings',
+          path: "home",
+          name: "home",
+          component: () => import("@/views/user/home/Home.vue")
+        },
+        {
+          path: "index",
+          name: "index",
+          component: () => import("@/views/user/index/Index.vue")
+        },
+        {
+          path: "settings",
+          name: "settings",
           // meta: { requireAuth: true },
-          component: () => import('@/views/user/settings/settings.vue')
+          component: () => import("@/views/user/settings/settings.vue")
+        },
+        {
+          path: "mess",
+          name: "mess",
+          component: () => import("@/views/user/mess/Mess.vue")
         }
       ]
     },
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
       component: login
     },
     {
-      path: '/register',
-      name: 'register',
+      path: "/register",
+      name: "register",
       component: login
     }
   ]
-})
+});
--- Wblog/src/views/user/user.vue
+++ Wblog/src/views/user/user.vue
@@ -2,145 +2,43 @@
   <div class="set-con main">
     <el-row>
       <el-col :span="6">
-        <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-          background-color="#aaa"
-          text-color="#fff"
-          active-text-color="#000"
-        >
-          <el-menu-item index="1">
-              <i class="el-icon-location"></i>
-              <span>我的主页</span>
+        <el-menu default-active="2" class="el-menu-vertical-demo" background-color="#aaa" text-color="#fff" active-text-color="#000" router>
+          <el-menu-item index="/user/home">
+            <i class="el-icon-location"></i>
+            <span>我的主页</span>
           </el-menu-item>
-          <el-menu-item index="2">
+          <el-menu-item index="/user/index">
             <i class="el-icon-menu"></i>
             <span slot="title">用户中心</span>
           </el-menu-item>
-          <el-menu-item index="3">
+          <el-menu-item index="/user/settings">
             <i class="el-icon-document"></i>
             <span slot="title">基本设置</span>
           </el-menu-item>
-          <el-menu-item index="4">
+          <el-menu-item index="/user/mess">
             <i class="el-icon-setting"></i>
             <span slot="title">我的消息</span>
           </el-menu-item>
         </el-menu>
       </el-col>
       <el-col :span="18">
-        <el-tabs v-model="activeName" @tab-click="handleClick">
-          <el-tab-pane label="我的资料" name="first">我的资料</el-tab-pane>
-          <el-tab-pane label="头像" name="avatar">
-            <el-upload
-              class="avatar-uploader"
-              :headers="headers"
-              :action="uploadPath"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload"
-            >
-              <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-          </el-tab-pane>
-          <el-tab-pane label="密码" name="third">密码</el-tab-pane>
-          <el-tab-pane label="账号绑定" name="fourth">账号绑定</el-tab-pane>
-        </el-tabs>
+        <router-view></router-view>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import { uploadPath } from "@/api/storage";
-import { getToken } from "@/utils/auth";
-export default {
-  data() {
-    return {
-      activeName: "second",
-      imageUrl: "",
-      uploadPath
-    };
-  },
-  computed: {
-    headers() {
-      return {
-        myToken: getToken()
-      };
-    }
-  },
-  methods: {
-    handleOpen(key, keyPath) {
-      // console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      // console.log(key, keyPath);
-    },
-    handleClick(tab, event) {
-      // console.log(tab, event);
-    },
-    handleAvatarSuccess(res, file) {
-      this.$message({
-        message: res.msg,
-        type: "success"
-      });
-      if (res.data) {
-        this.imageUrl = "/api" + res.data;
-        this.$store.getters.info.avatar = res.data;
-      }
-    },
-    beforeAvatarUpload(file) {
-      const isJPG = file.type === "image/jpeg";
-      const isPNG = file.type === "image/png";
-      const isLt2M = file.size / 1024 / 1024 < 2;
-      if (!isJPG && !isPNG) {
-        this.$message.error("上传头像图片格式不正确!");
-      }
-      if (!isLt2M) {
-        this.$message.error("上传头像图片大小不能超过 2MB!");
-      }
-      return (isJPG || isPNG) && isLt2M;
-    }
-  }
-};
 </script>
 
 <style lang="less">
-.set-con {
-  padding: 20px 0;
-  .el-row {
-    height: 500px;
-    .el-menu {
+  .set-con {
+    padding: 20px 0;
+    .el-row {
       height: 500px;
-    }
-    .el-tabs {
-      padding-left: 15px;
-    }
-    .avatar-uploader .el-upload {
-      border: 1px dashed #d9d9d9;
-      border-radius: 6px;
-      cursor: pointer;
-      position: relative;
-      overflow: hidden;
-      &:hover {
-        border-color: #409eff;
-      }
-      .avatar-uploader-icon {
-        font-size: 28px;
-        color: #8c939d;
-        width: 178px;
-        height: 178px;
-        line-height: 178px;
-        text-align: center;
-      }
-      .avatar {
-        width: 178px;
-        height: 178px;
-        display: block;
+      .el-menu {
+        height: 500px;
       }
     }
   }
-}
 </style>
